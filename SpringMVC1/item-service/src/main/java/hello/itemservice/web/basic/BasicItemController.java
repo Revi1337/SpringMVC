feb1da62a1d363cf5a166b129a6cc5ec8d4938b2
Revi1337 <david122123@gmail.com>
2023-06-26 17:50:13 +0900

    [Spring MVC - 상품 수정]

--- SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -53,9 +53,9 @@ public class BasicItemController {
      */
 //    @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam int quantity,
-                       Model model) {
+                            @RequestParam int price,
+                            @RequestParam int quantity,
+                            Model model) {
         Item item = new Item(itemName, price, quantity);
         itemRepository.save(item);
         model.addAttribute("item", item);
@@ -80,6 +80,7 @@ public class BasicItemController {
 
     /**
      * V3 - @ModelAttribute("item") 에 item 이라는 attr 를 설정하여, 자동으로 Model 에 item 이라는 이름의 attribute 를 추가시켜줌.
+     *
      * @param item
      * @return
      */
@@ -93,6 +94,7 @@ public class BasicItemController {
     /**
      * V4 - @ModelAttribute 에 attr 또한 설정하지않는 방법.
      * attr 을 설정하지 않으면, @ModelAttribute 를 달아준 Class 의 첫번쨰 글자만 소문자로 바꾼 item 이라는 이름으로 Model 에 담아줌
+     *
      * @param item
      * @return
      */
@@ -107,6 +109,7 @@ public class BasicItemController {
     /**
      * V5 - 이게 최종. @ModelAttribute 마저 삭제.
      * - @ModelAttribute 만 사라진것이기 떄문에 V4 와 동일하게 Item 클래스의 첫글자를 소문자로 바꾼 item 이름으로 Model 에 담아줌.
+     *
      * @param item
      * @return
      */
@@ -116,4 +119,18 @@ public class BasicItemController {
 
         return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
 }
