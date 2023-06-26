1069a247a8458e0e485b47ecf5602a65d849ed8f
Revi1337 <david122123@gmail.com>
2023-06-26 15:36:12 +0900

    [Spring MVC - 상품 상세]

--- SpringMVC1/item-service/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC1/item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,4 +1,4 @@
-package hello.itemservice.item;
+package hello.itemservice.domain.item;
 
 import lombok.Data;
 
--- SpringMVC1/item-service/src/test/java/hello/itemservice/item/ItemRepositoryTest.java
+++ SpringMVC1/item-service/src/test/java/hello/itemservice/item/ItemRepositoryTest.java
@@ -1,12 +1,13 @@
 package hello.itemservice.item;
 
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ItemRepositoryTest {
 
