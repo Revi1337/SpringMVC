bcebb19d8d48d904c61548df2fca252c89ffeff1
Revi1337 <david122123@gmail.com>
2023-06-26 16:16:58 +0900

    [Spring MVC - 상품 등록 폼]

--- SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller @RequestMapping("/basic/items") @RequiredArgsConstructor
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
@@ -35,5 +38,15 @@ public class BasicItemController {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
     }
 }
