362145935c8c2ea9ef197c148825d1e0a0c2119f
Revi1337 <david122123@gmail.com>
2023-06-26 18:02:43 +0900

    [Spring MVC - PRG Post/Redirect/Get]

--- SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ SpringMVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -117,7 +117,7 @@ public class BasicItemController {
     public String addItemV5(Item item) {
         itemRepository.save(item);
 
-        return "basic/item";
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
