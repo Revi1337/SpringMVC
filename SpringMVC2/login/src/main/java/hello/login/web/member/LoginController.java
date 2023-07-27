4a0cf6f2f71089d6a3b09bc1220411a4b7f9c86c
Revi1337 <david122123@gmail.com>
2023-07-27 21:27:50 +0900

    [Login Process - 쿠키 로그인]
    //  쿠키에는 영속 쿠키와 세션 쿠키가 있다.
    //  영속 쿠키: 만료 날짜를 입력하면 해당 날짜까지 유지
    //  세션 쿠키: 만료 날짜를 생략하면 브라우저 종료시 까지만 유지 (이름만 세션쿠키일뿐 세션과 관련없다.)
    //  쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
    //  쿠키를 만료시킬때는 쿠키값에 null 을 넣어주고, Max-Age 를 0 으로 설정해주면 된다.

--- SpringMVC2/login/src/main/java/hello/login/web/member/LoginController.java
+++ SpringMVC2/login/src/main/java/hello/login/web/member/LoginController.java
@@ -3,9 +3,12 @@ package hello.login.web.member;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.web.login.LoginForm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +28,9 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm loginForm,
+                        BindingResult bindingResult,
+                        HttpServletResponse httpServletResponse) {
         if (bindingResult.hasErrors())
             return "login/loginForm";
         Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
@@ -33,7 +38,16 @@ public class LoginController {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
-        // 로그인 성공 처리 TODO
+
+        //  쿠키에는 영속 쿠키와 세션 쿠키가 있다.
+        //  영속 쿠키: 만료 날짜를 입력하면 해당 날짜까지 유지
+        //  세션 쿠키: 만료 날짜를 생략하면 브라우저 종료시 까지만 유지 (이름만 세션쿠키이지 세션과 관련없다.)
+        //  쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+
+        httpServletResponse.addCookie(idCookie);
         return "redirect:/";
     }
 }
+
+//        ResponseCookie idCookie = ResponseCookie.from("memberId", String.valueOf(loginMember.getId())).build()
