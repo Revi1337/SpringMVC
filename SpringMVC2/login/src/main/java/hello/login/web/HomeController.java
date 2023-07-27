ca5f62418946d0e3cc6322fbc76167da8f6cf688
Revi1337 <david122123@gmail.com>
2023-07-28 00:13:53 +0900

    [Login Process - 서블릿 HTTP 세션2]
    // 스프링에서 제공하는 @SessionAttribute 기능은 세션을 생성하지 않음.
    //  로그인을 처음시도할때 URL 에 세션이 붙는 Tracking 모드를 해제해야 편함.
    `server.servlet.session.tracking-modes=cookie`

--- SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
+++ SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 
 @Controller @RequiredArgsConstructor @Slf4j
@@ -53,7 +53,7 @@ public class HomeController {
 
     // V3 ================================================================================================================================
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest httpServletRequest, Model model) {
 
         HttpSession session = httpServletRequest.getSession(false);
@@ -69,5 +69,18 @@ public class HomeController {
         return "loginHome";
     }
 
+    // V4 ================================================================================================================================
+
+    @GetMapping("/")    // 스프링에서 제공하는 @SessionAttribute 기능은 세션을 생성하지 않음.
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+                                    Model model) {
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) return "home";
+
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
 
 }
