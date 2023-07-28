8847b97e098194d3a9165bc35a4cb6f477f2ea7b
Revi1337 <david122123@gmail.com>
2023-07-28 18:37:48 +0900

    [Login Process2 - ArgumentResolver]

--- SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
+++ SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -71,10 +72,22 @@ public class HomeController {
 
     // V4 ================================================================================================================================
 
-    @GetMapping("/")    // 스프링에서 제공하는 @SessionAttribute 기능은 세션을 생성하지 않음.
+//    @GetMapping("/")    // 스프링에서 제공하는 @SessionAttribute 기능은 세션을 생성하지 않음.
     public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
                                     Model model) {
         // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) return "home";
+
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    // V5 ================================================================================================================================
+
+    @GetMapping("/")
+    public String homeLoginV5ArgumentResolver(@Login Member loginMember, Model model) {
         if (loginMember == null) return "home";
 
 
