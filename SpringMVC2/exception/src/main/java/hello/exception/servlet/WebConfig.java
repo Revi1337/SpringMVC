9bd5bdaab23eb683329e3142310661bf3a57a388
Revi1337 <david122123@gmail.com>
2023-07-29 16:55:05 +0900

    [Exception&Error - Servlet Exception 인터셉터]

--- SpringMVC2/exception/src/main/java/hello/exception/servlet/WebConfig.java
+++ SpringMVC2/exception/src/main/java/hello/exception/servlet/WebConfig.java
@@ -1,16 +1,28 @@
 package hello.exception.servlet;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+        // Interceptor 는 Filter 와 다르게 DispatcherType 을 지정해줄 수가 없다. 그 대신, 강력한 excludePathPatterns 로 빼주면 된다.
+    }
+
+//    @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
