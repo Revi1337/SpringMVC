ce20c834cb8d5b05ea8b9ca1216bf870f21916c0
Revi1337 <david122123@gmail.com>
2023-08-03 12:48:31 +0900

    [API 예외 처리 - 시작 (Servlet Exception 방식)]

---
+++ SpringMVC2/exception/src/main/java/hello/exception/api/APIExceptionController.java
@@ -0,0 +1,25 @@
+package hello.exception.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @Slf4j
+public class APIExceptionController {
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember(@PathVariable String id) {
+        if (id.equals("ex"))
+            throw new RuntimeException("잘못된 사용자");
+        return new MemberDto(id, "hello " + id);
+    }
+
+    @Data @AllArgsConstructor
+    public static class MemberDto {
+        private String memberId;
+        private String name;
+    }
+}
