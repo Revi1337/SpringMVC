208c9151f10748bdf9c6bd48546c7469c1ca8cd3
Revi1337 <david122123@gmail.com>
2023-08-12 14:09:56 +0900

    [FileUpload - Spring FileUpload & FileDownload]

---
+++ SpringMVC2/upload/src/main/java/hello/upload/file/FileStore.java
@@ -0,0 +1,59 @@
+package hello.upload.file;
+
+import hello.upload.domain.UploadFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.*;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        ArrayList<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty())
+            return null;
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFileName) {
+        String ext = extractedExt(originalFileName);
+        String randomFileName = UUID.randomUUID().toString();
+        return randomFileName + "." + ext;
+    }
+
+    private String extractedExt(String originalFilename) {
+        int lastCommaIndex = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(lastCommaIndex + 1);
+    }
+
+}
