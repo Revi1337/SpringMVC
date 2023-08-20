ee7377bb0dfefd453b4e2347367ec5b9366b954e
Revi1337 <david122123@gmail.com>
2023-08-20 11:07:24 +0900

    [TypeConverter - Spring 에 Formatter 적용]

--- SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 우선순위로 인한 주석 (Converter 가 우선순위가 높음)
+        // 또한, 주석친 친구들은 Number < -- > String 변환하는 Formatter 와 같은 역할이기 때문에 Converter 가 위에있으면 Formatter 가 동작하지 않게됨. 그래서 주석처리한 것.
+
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new IpPortToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
