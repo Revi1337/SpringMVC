ebb61b15f132028ffeab68c38957f7d60f97789f
Revi1337 <david122123@gmail.com>
2023-11-08 17:40:23 +0900

    [Validation FieldError, ObjectError]

--- SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -57,7 +57,7 @@ public class ValidationItemControllerV2 {
      * @param redirectAttributes
      * @return
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item,
                             BindingResult bindingResult, // 순서 중요. @ModelAttribute Item item 다음에 와야 한다.
                             RedirectAttributes redirectAttributes) {
@@ -76,6 +76,58 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,1000 원 이상이어야 합니다. 현재 값 = " + resultPrice));
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /**
+     * FieldError 는 두가지 생성자를 제공
+     * 1. FieldError(String objectName, String field, String defaultMessage)
+     * 2. FieldError(String objectName, String field, @Nullable Object rejectedValue, boolean bindingFailure, @Nullable String[] codes, @Nullable Object[] arguments, @Nullable String defaultMessage)
+     *
+     * - objectName : 오류가 발생한 객체 이름
+     * - field : 오류 필드
+     * - rejectedValue : 사용자가 입력한 값(거절된 값) --> 값 유지에 사용
+     * - bindingFailure : 타입 오류 같은 바인딩 실패인지(true), 바인딩은 성공했는데 검증 실패인지(false) 의 구분 값
+     * - codes : 메시지 코드
+     * - arguments : 메시지에서 사용하는 인자
+     * - defaultMessage : 기본 오류 메시지
+     * @param item
+     * @param bindingResult
+     * @param redirectAttributes
+     * @return
+     */
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes) {
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,1000 원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
