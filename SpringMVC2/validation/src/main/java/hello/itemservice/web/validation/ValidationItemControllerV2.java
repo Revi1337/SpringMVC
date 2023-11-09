857e58d94ad3dc7dfe68a9df7c77d623b1785d59
Revi1337 <david122123@gmail.com>
2023-11-09 16:26:49 +0900

    [Validation Validator 분리 1]

--- SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -217,7 +218,7 @@ public class ValidationItemControllerV2 {
      * @param redirectAttributes
      * @return
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item,
                             BindingResult bindingResult,
                             RedirectAttributes redirectAttributes) {
@@ -244,6 +245,33 @@ public class ValidationItemControllerV2 {
             }
         }
 
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /**
+     * Custom Validator 로 검증로직 분리
+     * @param item
+     * @param bindingResult
+     * @param redirectAttributes
+     * @return
+     */
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes) {
+        if (itemValidator.supports(item.getClass())) {
+            itemValidator.validate(item, bindingResult);
+        }
         // 검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
