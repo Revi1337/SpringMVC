231c34b2a7586a28361c89a935fd2d03a86ef053
Revi1337 <david122123@gmail.com>
2023-11-10 15:33:20 +0900

    [Bean Validation 시작]

--- SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,12 +1,21 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.NotBlank;  // jakarta.validation 어떤 구현체든 동작 (Java Validator, Hibernate Validator)
+import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;   // org.hibernate.validator 는 Hibernate Validator 에서만 동작.
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
     private Integer quantity;
 
